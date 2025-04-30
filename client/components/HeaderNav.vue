bb8a96359a462fc6b243da0cf3c65cbfba35e52e
Matthew Holloway <matthew@staff.ietf.org>
2025-04-30 16:59:51 +1200

    feat: after login return to same path via 'next' param

--- client/components/HeaderNav.vue
+++ client/components/HeaderNav.vue
@@ -123,7 +123,7 @@
               <DialogTitle class="mb-6 text-xs text-left font-bold text-gray-800 pl-3">
                 You're not logged in
               </DialogTitle>
-              <a href="/oidc/authenticate/" class="border bg-sky-900 px-4 py-2 text-white dark:text-neutral-300">
+              <a :href="`/oidc/authenticate/${nextParam}`" class="border bg-sky-900 px-4 py-2 text-white dark:text-neutral-300">
                 <span class="font-semibold">Login via Datatracker</span>
                 <Icon name="solar:alt-arrow-right-line-duotone" size="1.5em" class="ml-1" />
               </a>
@@ -146,6 +146,10 @@ import {
 import { useSiteStore } from '@/stores/site'
 import { useUserStore } from '@/stores/user'
 
+const route = useRoute()
+
+// mozilla_django_oidc supports a url param `next` to return to the current path https://mozilla-django-oidc.readthedocs.io/en/stable/settings.html?highlight=next#OIDC_REDIRECT_FIELD_NAME
+const nextParam = computed(() => route.fullPath ? `?next=${encodeURIComponent(route.fullPath)}` : '')
 const api = useApi()
 const csrf = useCookie('csrftoken', { sameSite: 'strict' })
 const buttonId = useId() // avoid a hydration error - see https://github.com/nuxt/ui/issues/1171
