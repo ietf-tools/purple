2eedf2df98e6c8c37edc38e513b3b45b01738b5f
Rudi Matz <rudi@staff.ietf.org>
2025-05-09 16:49:06 -0400

    feat: add is_active flag for RpcPerson

--- rpc/api.py
+++ rpc/api.py
@@ -111,6 +111,12 @@ class RpcPersonViewSet(viewsets.ReadOnlyModelViewSet, viewsets.GenericViewSet):
     serializer_class = RpcPersonSerializer
     queryset = RpcPerson.objects.all()
 
+    def get_queryset(self):
+        queryset = RpcPerson.objects.all()
+        if self.request.query_params.get("all") in ("true", "1"):
+            queryset = queryset.filter(is_active=True)
+        return queryset
+
     @with_rpcapi
     def get_serializer_context(self, rpcapi: rpcapi_client.DefaultApi):
         """Add context to the serializer"""
