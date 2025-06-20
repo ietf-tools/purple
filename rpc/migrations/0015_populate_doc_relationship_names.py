bf6aa10c894828e31cf94cd24142d02c1ae97fa0
Rudi Matz <rudi@staff.ietf.org>
2025-06-20 15:39:33 -0400

    add refqueue in populate-migration

--- rpc/migrations/0015_populate_doc_relationship_names.py
+++ rpc/migrations/0015_populate_doc_relationship_names.py
@@ -8,19 +8,22 @@ def forward(apps, schema_editor):
 
     DocRelationshipName.objects.create(
         slug="missref",
-        name="Missreference",
+        name="Missing reference",
         desc="Normative reference to a document that is still in draft state",
+        used=True,
+    )
+
+    DocRelationshipName.objects.create(
+        slug="refqueue",
+        name="Reference in Queue",
+        desc="Normative reference to a document that is in queue for publication",
         used=True,
     )
 
 
 def reverse(apps, schema_editor):
     DocRelationshipName = apps.get_model("rpc", "DocRelationshipName")
-    DocRelationshipName.objects.filter(
-        slug__in=[
-            "misref",
-        ]
-    ).delete()
+    DocRelationshipName.objects.filter(slug__in=["missref", "refqueue"]).delete()
 
 
 class Migration(migrations.Migration):
