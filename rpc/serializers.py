2f404271901f707f836c447851e755207bca12aa
Jennifer Richards <jennifer@staff.ietf.org>
2025-06-27 22:14:23 -0300

    fix: get name_map from context correctly
    This has been broken a loooong time...

--- rpc/serializers.py
+++ rpc/serializers.py
@@ -359,14 +359,15 @@ class RpcPersonSerializer(serializers.ModelSerializer):
         fields = ["id", "name", "hours_per_week", "capabilities", "roles", "is_active"]
 
     def __init__(self, *args, **kwargs):
-        self.name_map: dict[str, str] = kwargs.pop(
+        context = kwargs.get("context", {})
+        self.name_map: dict[int, str] = context.pop(
             "name_map", {}
         )  # datatracker_id -> name
         super().__init__(*args, **kwargs)
 
     def get_name(self, rpc_person) -> str:
         cached_name = self.name_map.get(
-            str(rpc_person.datatracker_person.datatracker_id), None
+            rpc_person.datatracker_person.datatracker_id, None
         )
         return cached_name or rpc_person.datatracker_person.plain_name
 
