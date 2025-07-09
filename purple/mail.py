404647dc489dfe3d733f8496dfbe8d08d55ce8b1
Kesara Rathnayake <kesara@fq.nz>
2025-07-09 23:57:50 +1200

    chore: Add ability to fail loudly

--- purple/mail.py
+++ purple/mail.py
@@ -4,11 +4,11 @@ from django.conf import settings
 from django.core.mail import send_mail as dj_send
 
 
-def send_mail(to, subject, msg, frm=None):
+def send_mail(to, subject, msg, frm=None, fail_silently=True):
     if not frm:
         frm = settings.DEFAULT_FROM_EMAIL
     if isinstance(to, str):
         to = [
             to,
         ]
-    dj_send(subject, msg, frm, to)
+    dj_send(subject, msg, frm, to, fail_silently=fail_silently)
