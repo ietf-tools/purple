f367371f15cc319f7617ef0e28485b5cb19adab1
Jennifer Richards <jennifer@staff.ietf.org>
2025-08-22 16:33:54 -0300

    style: normalize styling

--- client/pages/labels/index.vue
+++ client/pages/labels/index.vue
@@ -5,7 +5,11 @@
         <h1 class="text-base font-semibold leading-6 text-gray-900 dark:text-neutral-300">Labels</h1>
       </div>
       <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-        <button type="button" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" @click="addLabel()">Add Label</button>
+        <button
+          type="button"
+          class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          @click="addLabel()">Add Label
+        </button>
       </div>
     </div>
     <ErrorAlert v-if="labelsError" title="API Error">
@@ -17,20 +21,25 @@
           <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
             <table class="min-w-fit divide-y divide-gray-300">
               <thead class="bg-gray-50">
-                <tr>
-                  <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Name</th>
-                  <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                    <span class="sr-only">Edit</span>
-                  </th>
-                </tr>
+              <tr>
+                <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Name</th>
+                <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                  <span class="sr-only">Edit</span>
+                </th>
+              </tr>
               </thead>
               <tbody class="divide-y divide-gray-200 bg-white">
-                <tr v-for="label in sortedLabels" :key="label.slug">
-                  <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6"><RpcLabel :label="label"/></td>
-                  <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                    <Icon name="circum:edit" class="text-indigo-600 hover:text-indigo-900 cursor-pointer"  @click="editLabel(label)" /><span class="sr-only">Edit {{ label.slug }}</span>
-                  </td>
-                </tr>
+              <tr v-for="label in sortedLabels" :key="label.slug">
+                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                  <RpcLabel :label="label"/>
+                </td>
+                <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                  <Icon
+                    name="circum:edit" class="text-indigo-600 hover:text-indigo-900 cursor-pointer"
+                    @click="editLabel(label)"/>
+                  <span class="sr-only">Edit {{ label.slug }}</span>
+                </td>
+              </tr>
               </tbody>
             </table>
           </div>
@@ -61,7 +70,7 @@ if (!val) {
 }
 const { openOverlayModal } = val
 
-async function addLabel () {
+async function addLabel() {
   try {
     // Empty componentProps => create a new label
     await openOverlayModal({ component: RpcLabelEditDialog })
@@ -83,7 +92,7 @@ async function addLabel () {
   }
 }
 
-async function editLabel (label: Label) {
+async function editLabel(label: Label) {
   try {
     await openOverlayModal({
       component: RpcLabelEditDialog,
