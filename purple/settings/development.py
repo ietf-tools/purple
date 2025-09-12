1b18f604d734650cb8af72d7568ad016ba303708
Jennifer Richards <jennifer@staff.ietf.org>
2025-09-12 16:32:00 -0300

    chore(dev): replace mailhog with mailpit

--- purple/settings/development.py
+++ purple/settings/development.py
@@ -57,7 +57,7 @@ DATABASES = {
 
 # email
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = os.getenv("EMAIL_HOST", "mailhog")
+EMAIL_HOST = os.getenv("EMAIL_HOST", "mailpit")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 1025))
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "purple@rfc-editor.org")
 
