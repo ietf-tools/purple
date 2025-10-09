8918c8abdda73f5cc58868868639240eb512ebc0
Rudi Matz <rudi@staff.ietf.org>
2025-10-09 12:53:30 -0400

    ruff

--- rpc/migrations/0017_assignment_unique_active_assignment_per_person_rfc_role.py
+++ rpc/migrations/0017_assignment_unique_active_assignment_per_person_rfc_role.py
@@ -4,14 +4,21 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('rpc', '0016_alter_assignment_state_and_more'),
+        ("rpc", "0016_alter_assignment_state_and_more"),
     ]
 
     operations = [
         migrations.AddConstraint(
-            model_name='assignment',
-            constraint=models.UniqueConstraint(condition=models.Q(('state__in', ['done', 'withdrawn', 'closed_for_hold']), _negated=True), fields=('person', 'rfc_to_be', 'role'), name='unique_active_assignment_per_person_rfc_role', violation_error_message='A person can only have one active assignment per RFC and role'),
+            model_name="assignment",
+            constraint=models.UniqueConstraint(
+                condition=models.Q(
+                    ("state__in", ["done", "withdrawn", "closed_for_hold"]),
+                    _negated=True,
+                ),
+                fields=("person", "rfc_to_be", "role"),
+                name="unique_active_assignment_per_person_rfc_role",
+                violation_error_message="A person can only have one active assignment per RFC and role",
+            ),
         ),
     ]
