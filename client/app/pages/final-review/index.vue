1537b5d60d173205795f9c63de75dc1e4fdace9f
Matthew Holloway <matthew@holloway.co.nz>
2025-11-07 10:41:56 +1300

    feat: final review prep

--- client/app/pages/final-review/index.vue
+++ client/app/pages/final-review/index.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts">
-definePageMeta({
-  middleware: [
-    function (to, from) {
-      return navigateTo('/auth48/inprogress')
-    }
-  ]
-})
-</script>
+<template>
+  <div>
+    <TitleBlock
+      title="Final Review (formerly AUTH48)"
+      summary="Where the magic mercifully stops happening."/>
+
+    <FinalReviewInProgress />
+    <FinalReviewDone />
+    <FinalReviewForPublication />
+  </div>
+</template>
