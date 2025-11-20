5275adcf83294019933a72e486bf110145d9ff32
Matthew Holloway <matthew@holloway.co.nz>
2025-11-20 17:58:54 +1300

    feat: clusters homepage w/ filter

--- client/app/components/DocumentCardMini.vue
+++ client/app/components/DocumentCardMini.vue
@@ -1,12 +1,9 @@
 <template>
   <li class="whitespace-nowrap inline-block">
-    <a :href="`/docs/${props.document.name}`" :class="[
-      ANCHOR_STYLE,
-      'inline-block p-1 rounded-md'
-    ]">
+    <a :href="`/docs/${props.document.name}`" class="text-gray-700 dark:text-gray-200 font-semibold no-underline hover:underline focus:underline inline-block p-1 rounded-md">
       <span v-if="props.document.rfcNumber">
         <span class="font-normal">RFC{{ NBSP }}</span>
-        <span class="font-bold">{{ `${props.document.rfcNumber}:${SPACE} ` }}</span>
+        <span class="font-semibold">{{ `${props.document.rfcNumber}:${SPACE} ` }}</span>
       </span>
       <span class="font-mono font-normal">
         {{ props.document.name }}
--- client/app/pages/clusters/index.vue
+++ client/app/pages/clusters/index.vue
@@ -12,14 +12,22 @@
       </template>
     </TitleBlock>
 
-    <div class="flex flex-col gap-5 mt-5">
-      <div v-for="cluster in clusters" :key="cluster.number" class="flex flex-row items-start">
-        <h2 class="flex flex-row items-start text-lg whitespace-nowrap grow-0 shrink-0 w-[7em]">
+    <form class="flex flex-row items-center w-[200px] pt-5">
+      <label class="text-gray-900 text-sm font-bold mr-1" for="filterInput">Filter:</label>
+      <input type="text" inputmode="numeric" pattern="[0-9]*" id="filterInput" v-model="filterValueString"
+        class="focus:shadow-gray-300 inline-flex w-full flex-1 items-center justify-center rounded-lg px-3 text-sm leading-none outline-none border-gray-500 text-gray-900"
+        placeholder="E.g. 20" />
+    </form>
+
+    <div class="flex flex-col gap-3 mt-5 mb-10">
+      <div v-for="cluster in filteredClusters" :key="cluster.number"
+        class="flex flex-row items-start border border-gray-300 shadow-md rounded-md px-2 py-1">
+        <h2 class="flex flex-row items-start text-lg whitespace-nowrap grow-0 shrink-0 w-[7em] px-2">
           <a :href="`/clusters/${cluster.number}/`" :class="ANCHOR_STYLE">
             Cluster {{ cluster.number }}
           </a>
         </h2>
-        <ul class="flex flex-wrap gap-2 text-xs">
+        <ul class="flex flex-wrap pt-1 gap-y-1 gap-x-3 text-xs leading-[1]">
           <DocumentCardMini v-for="document in cluster.documents" :document="document" />
         </ul>
       </div>
@@ -43,6 +51,8 @@ const state = reactive({
   notifDialogMessage: ''
 })
 
+const filterValueString = ref('')
+
 const api = useApi()
 
 const { data: clusters, pending, refresh } = await useAsyncData(
@@ -51,4 +61,7 @@ const { data: clusters, pending, refresh } = await useAsyncData(
   {}
 )
 
+const filteredClusters = computed(() => clusters.value?.filter(
+  cluster => cluster.number.toString().includes(filterValueString.value.trim())
+) ?? [])
 </script>
