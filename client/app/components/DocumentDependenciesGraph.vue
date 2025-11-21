82b21d239cc45a9d660da4803671d8efe62261bc
Matthew Holloway <matthew@holloway.co.nz>
2025-11-21 18:04:18 +1300

    feat: cluster into page

--- client/app/components/DocumentDependenciesGraph.vue
+++ client/app/components/DocumentDependenciesGraph.vue
@@ -1,47 +1,37 @@
 <template>
-  <div class="h-full flex flex-col">
-    <div class="flex gap-2 justify-between p-2 border-b border-gray-400">
-      <span class="flex flex-row items-center text-md font-bold pl-1">
-        Document dependencies
-      </span>
-      <span class="flex flex-row gap-2">
-        <BaseButton btn-type="cancel" @click="closeOverlayModal" aria-label="Close modal">
-          &times;
-        </BaseButton>
-      </span>
-    </div>
-    <div ref="container" class="flex-1 overflow-hidden"></div>
-    <div class="flex gap-2 justify-between border-t border-gray-400 p-2">
-      <span class="flex flex-row items-center text-sm pl-1">
-        Pan and zoom the dependency graph after the layout settles.
-      </span>
-      <span class="flex flex-row items-center gap-2">
-        <RpcCheckbox label="Show legend" :value="true" :checked="showLegend" @change="showLegend = !showLegend"
-          size='medium' class="mr-3" />
-        <BaseButton btn-type="default" :class="{ 'opacity-50': !canDownload }" @click="handleDownload">
-          <span v-if="canDownload">
-            <Icon name="el:download-alt" size="1.1em" class="mr-2" />
-            Download
-          </span>
-          <span v-else>
-            <Icon name="ei:spinner-3" size="1.5em" class="animate-spin mr-2" />
-            Loading...
-          </span>
-        </BaseButton>
-        <BaseButton btn-type="cancel" @click="closeOverlayModal" aria-label="Close modal">
-          Cancel
-        </BaseButton>
-      </span>
-    </div>
+  <h2 class="flex flex-row items-center text-md font-bold pl-1">
+    Document dependencies
+  </h2>
+
+  <div ref="container" class="overflow-hidden h-full">
+  </div>
+
+  <div class="flex gap-2 justify-between border-t border-gray-400 p-2">
+    <span class="flex flex-row items-center text-sm pl-1">
+      Pan and zoom the dependency graph after the layout settles.
+    </span>
+    <span class="flex flex-row items-center gap-2">
+      <RpcCheckbox label="Show legend" :value="true" :checked="showLegend" @change="showLegend = !showLegend"
+        size='medium' class="mr-3" />
+      <BaseButton btn-type="default" :class="{ 'opacity-50': !canDownload }" @click="handleDownload">
+        <span v-if="canDownload">
+          <Icon name="el:download-alt" size="1.1em" class="mr-2" />
+          Download
+        </span>
+        <span v-else>
+          <Icon name="ei:spinner-3" size="1.5em" class="animate-spin mr-2" />
+          Loading...
+        </span>
+      </BaseButton>
+    </span>
   </div>
 </template>
 
 <script setup lang="ts">
-import { overlayModalKey } from '~/providers/providerKeys';
-import type { Cluster } from '~/purple_client'
+import type { Cluster, RfcToBe } from '~/purple_client'
 import { draw_graph, type DrawGraphParameters } from '~/utils/document_relations';
 import { legendData, test_data2 } from '~/utils/document_relations-utils'
-import type { DataParam, NodeParam, LinkParam } from '~/utils/document_relations-utils';
+// import type { DataParam, NodeParam, LinkParam } from '~/utils/document_relations-utils';
 import { downloadTextFile } from '~/utils/download';
 import { assert } from '~/utils/typescript';
 
@@ -50,14 +40,6 @@ type Props = {
 }
 
 const props = defineProps<Props>()
-
-const overlayModalKeyInjection = inject(overlayModalKey)
-
-if (!overlayModalKeyInjection) {
-  throw Error('Expected injection of overlayModalKey')
-}
-
-const { closeOverlayModal } = overlayModalKeyInjection
 
 const api = useApi()
 
@@ -102,79 +84,86 @@ const { data: rfcToBes, error: rfcToBesError } = await useAsyncData(
             return api.documentsRetrieve({ draftName })
           })
         }
-      ) ?? [])
+      ) ?? []),
+  {
+    lazy: true,
+    server: false,
+    default: () => [] as RfcToBe[]
+  }
 )
 
 const canDownload = computed(() => Boolean(rfcToBes.value))
 
-const data = computed((): DataParam => {
-  return {
-    links: [
-      ...documentsReferencesByRfcs.value?.map((documentsReferencesByRfc): LinkParam => {
-        const { rfcNumber, draftName } = documentsReferencesByRfc
+// const data = computed((): DataParam => {
+//   return {
+//     links: [
+//       ...documentsReferencesByRfcs.value?.map((documentsReferencesByRfc): LinkParam => {
+//         const { rfcNumber, draftName } = documentsReferencesByRfc
 
-        assert(rfcNumber)
-        assert(draftName)
+//         assert(rfcNumber)
+//         assert(draftName)
 
-        return {
-          source: `rfc${rfcNumber}`,
-          rel: 'relinfo', //
-          target: draftName,
-        }
-      }) ?? [],
+//         return {
+//           source: `rfc${rfcNumber}`,
+//           rel: 'relinfo', //
+//           target: draftName,
+//         }
+//       }) ?? [],
 
-      ...documentsReferencesByRfcs.value?.flatMap((documentsReferencesByRfc): LinkParam[] => {
-        const { rfcNumber, draftName, documentsReferences } = documentsReferencesByRfc
+//       ...documentsReferencesByRfcs.value?.flatMap((documentsReferencesByRfc): LinkParam[] => {
+//         const { rfcNumber, draftName, documentsReferences } = documentsReferencesByRfc
 
-        return documentsReferences.map((documentsReference): LinkParam => {
-          const {
-            draftName: source,
-            targetDraftName: target
-          } = documentsReference
+//         return documentsReferences.map((documentsReference): LinkParam => {
+//           const {
+//             draftName: source,
+//             targetDraftName: target
+//           } = documentsReference
 
-          assert(source)
-          assert(target)
+//           assert(source)
+//           assert(target)
 
-          return {
-            source,
-            rel: 'refnorm',
-            target,
-          }
-        })
-      }) ?? []
-    ],
-    nodes: [
-      ...rfcToBes.value?.map((rfcToBe): NodeParam => {
-        const {
-          name: id,
-          intendedStdLevel: level,
-        } = rfcToBe
+//           return {
+//             source,
+//             rel: 'refnorm',
+//             target,
+//           }
+//         })
+//       }) ?? []
+//     ],
+//     nodes: [
+//       ...rfcToBes.value?.map((rfcToBe): NodeParam => {
+//         const {
+//           name: id,
+//           intendedStdLevel: level,
+//         } = rfcToBe
 
-        assert(id)
-        assert(level)
+//         assert(id)
+//         assert(level)
 
-        return {
-          id,
-          url: `/doc/${id}/`,
-          rfc: true,
-          "post-wg": undefined,
-          expired: undefined,
-          replaced: undefined,
-          group: undefined,
-          level: undefined,
-        }
-      }) ?? []
-    ]
-  }
-})
+//         return {
+//           id,
+//           url: `/doc/${id}/`,
+//           rfc: true,
+//           "post-wg": undefined,
+//           expired: undefined,
+//           replaced: undefined,
+//           group: undefined,
+//           level: undefined,
+//         }
+//       }) ?? []
+//     ]
+//   }
+// })
 
-watchEffect(() => {
+onMounted(() => {
   const { value: container } = containerRef
 
   if (!container) {
     console.error('container ref not found')
     return
   }
+
+  console.log("container ref found")
 
   const args: DrawGraphParameters = showLegend.value ? [legendData, "this group"] : [test_data2, "stir"]
 
