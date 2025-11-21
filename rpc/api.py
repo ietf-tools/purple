d494ae6cf4371c6281c4c4c1ec64c7bcc89c252d
Rudi Matz <rudi@staff.ietf.org>
2025-11-21 13:12:36 -0500

    ruff

--- rpc/api.py
+++ rpc/api.py
@@ -235,9 +235,7 @@ class RpcPersonAssignmentViewSet(mixins.ListModelMixin, viewsets.GenericViewSet)
     TODO: permissions
     """
 
-    queryset = Assignment.objects.exclude(
-        state__in=ASSIGNMENT_INACTIVE_STATES
-    )
+    queryset = Assignment.objects.exclude(state__in=ASSIGNMENT_INACTIVE_STATES)
     serializer_class = NestedAssignmentSerializer
 
     def get_queryset(self):
