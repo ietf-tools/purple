499829f1f1534ddb37e13f18fd44dff22271dd32
Jennifer Richards <jennifer@staff.ietf.org>
2025-11-25 13:28:24 -0400

    chore: better celery logging

--- purple/celery.py
+++ purple/celery.py
@@ -34,4 +34,15 @@ def debug_task():
     stdout.write(
         f"debug_task executed at {datetime.datetime.now(tz=datetime.UTC).isoformat()}\n"
     )
-    stdout.flush()
+
+
+@app.task(ignore_result=True)
+def debug_log_task():
+    import datetime
+
+    from celery.utils.log import get_task_logger
+
+    get_task_logger(__name__).info(
+        "debug_log_task executed at "
+        + datetime.datetime.now(tz=datetime.UTC).isoformat()
+    )
