6e259adecd44d46e253faf79e7293b0e1aa3767c
Kesara Rathnayake <kesara@fq.nz>
2025-11-26 21:00:50 +1300

    test: Add tests for _generate_crossref_xml

--- purple/tests_crossref.py
+++ purple/tests_crossref.py
@@ -2,10 +2,12 @@
 
 from xml.etree.ElementTree import tostring
 
+from django.conf import settings
 from django.test import TestCase, override_settings
+from django.utils import timezone
 
-from purple.crossref import _get_contributors, _get_name_parts
-from rpc.factories import RfcAuthorFactory
+from purple.crossref import _generate_crossref_xml, _get_contributors, _get_name_parts
+from rpc.factories import RfcAuthorFactory, RfcToBeFactory, StreamNameFactory
 
 
 class CrossrefTests(TestCase):
@@ -59,3 +61,61 @@ class CrossrefTests(TestCase):
         self.assertIn("first", xml_str)
         self.assertIn("editor", xml_str)
         self.assertIn("<surname>Undómiel</surname>", xml_str)
+
+    @override_settings(
+        DOI_REGISTRANT="Mordor",
+        DOI_DEPOSITOR="Sauron du Mordor",
+        DOI_PREFIX="10.17487",
+        DOI_EMAIL="sauron@example.org",
+        DOI_URL="https://www.rfc-editor.org/scrolls/",
+    )
+    def test_generate_crossref_xml(self):
+        RFC = 12345
+        PUBLISHED = timezone.now()
+        stream = StreamNameFactory.create(slug="ietf", name="IETF")
+        rfc = RfcToBeFactory.create(
+            rfc_number=RFC, published_at=PUBLISHED, submitted_stream=stream
+        )
+        rfc.draft.title = "The Book of Mazarbul"
+        rfc.draft.save()
+        rfc_author_1 = RfcAuthorFactory.create(
+            titlepage_name="A. Undómiel", is_editor=True
+        )
+        rfc_author_2 = RfcAuthorFactory.create(
+            titlepage_name="L. Greenleaf", is_editor=False
+        )
+        rfc.authors.set([rfc_author_1, rfc_author_2])
+
+        xml = _generate_crossref_xml(RFC)
+        xml_str = tostring(xml, encoding="unicode")
+
+        # registrant
+        self.assertIn(
+            f"<registrant_name>{settings.DOI_REGISTRANT}</registrant_name>", xml_str
+        )
+
+        # depositor
+        self.assertIn(
+            f"<depositor_name>{settings.DOI_DEPOSITOR}</depositor_name>", xml_str
+        )
+        self.assertIn(f"<email_address>{settings.DOI_EMAIL}</email_address>", xml_str)
+
+        # first author
+        first_author = xml.findall(".//person_name[@sequence='first']")
+        self.assertEqual(len(first_author), 1)
+        self.assertEqual(first_author[0].find(".//surname").text, "Undómiel")
+        self.assertEqual(first_author[0].get("contributor_role"), "editor")
+
+        # second author
+        second_author = xml.findall(".//person_name[@sequence='additional']")
+        self.assertEqual(len(second_author), 1)
+        self.assertEqual(second_author[0].find(".//surname").text, "Greenleaf")
+        self.assertEqual(second_author[0].get("contributor_role"), "author")
+
+        # RFC metada
+        self.assertIn(f"<title>{rfc.draft.title}</title>", xml_str)
+        self.assertIn(f"<month>{PUBLISHED.month}</month>", xml_str)
+        self.assertIn(f"<year>{PUBLISHED.year}</year>", xml_str)
+        self.assertIn(f"<item_number>rfc{RFC}</item_number>", xml_str)
+        self.assertIn(f"<doi>{settings.DOI_PREFIX}/rfc{RFC}</doi>", xml_str)
+        self.assertIn(f"<resource>{settings.DOI_URL}/rfc{RFC}</resource>", xml_str)
