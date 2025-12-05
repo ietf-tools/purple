7332a0971cb93e2eeded44f0b41f255decd00e9d
Rudi Matz <rudi@staff.ietf.org>
2025-12-05 09:23:21 -0500

    ruff

--- rpc/migrations/0026_alter_rfctobe_rfc_number.py
+++ rpc/migrations/0026_alter_rfctobe_rfc_number.py
@@ -1,19 +1,24 @@
 # Copyright The IETF Trust 2025, All Rights Reserved
 
-import rpc.models
 from django.db import migrations, models
+
+import rpc.models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('rpc', '0025_alter_historicalrfctobe_iana_status_and_more'),
+        ("rpc", "0025_alter_historicalrfctobe_iana_status_and_more"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='rfctobe',
-            name='rfc_number',
-            field=models.PositiveIntegerField(blank=True, null=True, unique=True, validators=[rpc.models.validate_not_unusable_rfc_number]),
+            model_name="rfctobe",
+            name="rfc_number",
+            field=models.PositiveIntegerField(
+                blank=True,
+                null=True,
+                unique=True,
+                validators=[rpc.models.validate_not_unusable_rfc_number],
+            ),
         ),
     ]
