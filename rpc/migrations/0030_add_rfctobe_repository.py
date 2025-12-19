0120bf00f16b008c528ed63d62eea7b67347a099
Jennifer Richards <jennifer@staff.ietf.org>
2025-12-19 12:54:35 -0400

    chore: renumber migrations

--- rpc/migrations/0030_add_rfctobe_repository.py
+++ rpc/migrations/0030_add_rfctobe_repository.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("rpc", "0026_alter_rfctobe_rfc_number"),
+        ("rpc", "0029_historicalrfctobeblockingreason_and_more"),
     ]
 
     operations = [
