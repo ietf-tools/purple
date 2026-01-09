31da7e15e6570487740ba95fbbbe87ec800c4446
Rudi Matz <rudi@staff.ietf.org>
2026-01-09 09:26:26 -0500

    ruff

--- rpc/migrations/0032_alter_mailmessage_cc_alter_mailmessage_message_id_and_more.py
+++ rpc/migrations/0032_alter_mailmessage_cc_alter_mailmessage_message_id_and_more.py
@@ -6,30 +6,29 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('rpc', '0031_mailmessage'),
+        ("rpc", "0031_mailmessage"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='mailmessage',
-            name='cc',
+            model_name="mailmessage",
+            name="cc",
             field=rpc.models.AddressListField(blank=True, max_length=1000),
         ),
         migrations.AlterField(
-            model_name='mailmessage',
-            name='message_id',
+            model_name="mailmessage",
+            name="message_id",
             field=models.CharField(default=purple.mail.make_message_id, max_length=255),
         ),
         migrations.AlterField(
-            model_name='mailmessage',
-            name='subject',
+            model_name="mailmessage",
+            name="subject",
             field=models.CharField(max_length=1000),
         ),
         migrations.AlterField(
-            model_name='mailmessage',
-            name='to',
+            model_name="mailmessage",
+            name="to",
             field=rpc.models.AddressListField(max_length=1000),
         ),
     ]
