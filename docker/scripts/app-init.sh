63f3651cac9c08449e9a129a56314ee533cc7840
Jennifer Richards <jennifer@staff.ietf.org>
2026-02-17 21:09:05 -0400

    chore(dev): untangle api build / deps

--- docker/scripts/app-init.sh
+++ docker/scripts/app-init.sh
@@ -8,6 +8,18 @@ git config --global --add safe.directory /workspace
 # Turn off git info in zsh prompt (causes slowdowns)
 git config oh-my-zsh.hide-info 1
 
+# Try to fetch datatracker API schema and build the client
+echo "Fetching datatracker API schema..."
+if wget -O rpcapi.yaml http://host.docker.internal:8000/api/schema/; then
+    echo "Building datatracker API client..."
+    npx --yes @openapitools/openapi-generator-cli@2.28 generate  --generator-key datatracker # config in openapitools.json
+    /usr/bin/mkdir -p openapi/rpcapi_client/
+    /bin/cp rpcapi.yaml openapi/rpcapi_client/.rpcapi.yaml
+    BUILT_API=yes
+else
+    echo "...API schema fetch failed"
+fi
+
 # Install requirements.txt dependencies
 echo "Installing dependencies from requirements.txt..."
 pip3 --disable-pip-version-check --no-cache-dir install --user --no-warn-script-location -r requirements.txt
@@ -20,11 +32,6 @@ sudo nginx
 echo "Waiting for DB container to come online..."
 /usr/local/bin/wait-for db:5432 -- echo "PostgreSQL ready"
 
-# Install client dependencies
-cd client
-npm install
-cd ..
-
 # Run migrations
 echo "Running migrations..."
 ./manage.py migrate --no-input || true
@@ -35,25 +42,20 @@ echo "Populating initial history..."
 # Collect statics
 ./manage.py collectstatic --no-input || true
 
-# Try to fetch datatracker API schema and build the client
-echo "Fetching datatracker API schema..."
-if wget -O rpcapi.yaml http://host.docker.internal:8000/api/schema/; then
-    echo "Building datatracker API client..."
-    client/node_modules/.bin/openapi-generator-cli generate  --generator-key datatracker # config in openapitools.json
-    /bin/cp rpcapi.yaml openapi/rpcapi_client/.rpcapi.yaml
-    BUILT_API=yes
-else
-    echo "...API schema fetch failed"
-fi
-
 # Django should be operational now. Build purple API client.
 ./manage.py spectacular --file purple_api.yaml && \
-    client/node_modules/.bin/openapi-generator-cli generate --generator-key purple  || true
-    echo "If not set, add @ts-nocheck in runtime.ts to avoid type errors from generated code"
-    if ! grep -q "// @ts-nocheck" "client/app/purple_client/runtime.ts"; then
-        sed -i '1i // @ts-nocheck' "client/app/purple_client/runtime.ts"
-    fi
-    /bin/cp purple_api.yaml client/app/purple_client/.purple_api.yaml
+    npx --yes @openapitools/openapi-generator-cli@2.28 generate --generator-key purple  || true
+# If not set, add @ts-nocheck in runtime.ts to avoid type errors from generated code
+if ! grep -q "// @ts-nocheck" "client/app/purple_client/runtime.ts"; then
+    sed -i '1i // @ts-nocheck' "client/app/purple_client/runtime.ts"
+fi
+/usr/bin/mkdir -p client/app/purple_client
+/bin/cp purple_api.yaml client/app/purple_client/.purple_api.yaml
+
+# Install client dependencies
+cd client
+npm install
+cd ..
 
 sudo touch /.dev-ready
 
