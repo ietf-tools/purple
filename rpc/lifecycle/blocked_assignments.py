724b6e7db15e235fef8d8d2277d18a2e8600e228
Rudi Matz <rudi@staff.ietf.org>
2026-02-24 13:27:39 -0500

    fix: finetune block reasons publish

--- rpc/lifecycle/blocked_assignments.py
+++ rpc/lifecycle/blocked_assignments.py
@@ -138,14 +138,27 @@ def get_block_reasons(rfc: RfcToBe) -> set[str]:
             reasons.add(BlockingReason.LABEL_IANA_HOLD)
         if rfc.labels.filter(slug__in=["Tools Issue"]).exists():
             reasons.add(BlockingReason.TOOLS_ISSUE)
-        # any document this draft normatively references has not been published
+        # any document this draft normatively references is not ready for publication
         refqueue_qs = rfc.rpcrelateddocument_set.filter(relationship="refqueue")
         if refqueue_qs.exists():
             for ref in refqueue_qs:
-                incomplete_publish_qs = (
-                    ref.target_rfctobe.incomplete_activities().filter(slug="publisher")
+                # block if publisher is neither done, active, or pending
+                publisher_done_qs = ref.target_rfctobe.assignment_set.filter(
+                    role__slug="publisher", state=Assignment.State.DONE
+                ).exists()
+                publisher_active_qs = (
+                    ref.target_rfctobe.assignment_set.filter(role__slug="publisher")
+                    .active()
+                    .exists()
                 )
-                if incomplete_publish_qs.exists():
+                publisher_pending_qs = (
+                    ref.target_rfctobe.pending_activities()
+                    .filter(slug="publisher")
+                    .exists()
+                )
+                if not (
+                    publisher_done_qs or publisher_active_qs or publisher_pending_qs
+                ):
                     reasons.add(BlockingReason.REFQUEUE_PUBLISH_INCOMPLETE)
         if rfc.finalapproval_set.active().exists():
             reasons.add(BlockingReason.FINAL_APPROVAL_PENDING)
