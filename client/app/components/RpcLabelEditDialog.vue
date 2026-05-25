10f667d27b392f20f96dbaf8526fd731c259679d
Rudi Matz <rudi@staff.ietf.org>
2026-05-25 15:44:05 -0400

    no backtick

--- client/app/components/RpcLabelEditDialog.vue
+++ client/app/components/RpcLabelEditDialog.vue
@@ -37,7 +37,7 @@
               <input
                 id="slug" v-model="label.slug" type="text" name="slug"
                 class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                placeholder="e.g. `markdown`">
+                placeholder="e.g. 'markdown'">
             </div>
           </div>
         </div>
